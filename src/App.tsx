1ff69dc919fe004e6fc855973bdd613375b67c1c
DanielleColucci <ddcolucci99@gmail.com>
2023-02-24 15:20:49 -0500

    pass user as prop to ProfileDetails

--- src/App.tsx
+++ src/App.tsx
@@ -130,7 +130,7 @@ function App(): JSX.Element {
           path="/profiles/:id"
           element={
             <ProtectedRoute user={user}>
-              <ProfileDetails />
+              <ProfileDetails user={user}/>
             </ProtectedRoute>
           }
         />
