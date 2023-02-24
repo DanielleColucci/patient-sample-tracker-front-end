3e32c88be4792c836ff3d107d69bb86124696704
DanielleColucci <ddcolucci99@gmail.com>
2023-02-24 09:38:26 -0500

    fetch current sample and store in state in SampleDetails

--- src/pages/SampleDetails/SampleDetails.tsx
+++ src/pages/SampleDetails/SampleDetails.tsx
@@ -1,4 +1,32 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router';
+
+// services
+import * as sampleService from '../../services/sampleService'
+
+// types 
+import { Sample } from "../../types/models"
+
 const SampleDetails = (): JSX.Element => {
+  const { id } = useParams()
+  const [sample, setSample] = useState<Sample | null>(null)
+
+  useEffect((): void => {
+    const fetchSample = async (): Promise<void> => {
+      try {
+        if (id) {
+          const sampleData = await sampleService.show(parseInt(id))
+          setSample(sampleData)
+        } else {
+          return
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchSample()
+  }, [id])
+
   return (
     <h1>sample details page!</h1>
   )
