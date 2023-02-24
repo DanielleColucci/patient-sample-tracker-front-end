7417450871ec94277288c2042bd642ab7bf7460c
DanielleColucci <ddcolucci99@gmail.com>
2023-02-24 10:03:08 -0500

    add details link to SampleCard

--- src/components/SampleCard/SampleCard.tsx
+++ src/components/SampleCard/SampleCard.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 // types
 import { Sample } from "../../types/models"
 
@@ -13,6 +15,9 @@ const SampleCard = (props: SampleCardProps): JSX.Element => {
       <h2>{sample.sampleNumber}</h2>
       <h4>{sample.date}</h4>
       <h4>{sample.Profile?.name}</h4>
+      <Link to={`/samples/${sample.id}`}>
+        Details
+      </Link>
     </div>
   )
 }
