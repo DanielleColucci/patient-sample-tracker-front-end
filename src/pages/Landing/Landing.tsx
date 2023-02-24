3b0278be710c06469163e525648557fe99b050a9
DanielleColucci <ddcolucci99@gmail.com>
2023-02-24 17:27:52 -0500

    update landing page styling

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -13,11 +13,11 @@ const Landing = (props: LandingProps): JSX.Element => {
 
   return (
     <main className={styles.container}>
-      <h1>Welcome</h1>
+      <h1 className={styles.welcome}>Welcome</h1>
       {!user && 
         <>
-          <h2>Please log in to continue.</h2>
-          <h3>Don't have an account? <a href="/signup">Sign up</a> to request access.
+          <h2>Please <a className={styles.link} href="/login">log in</a> to continue.</h2>
+          <h3>Don't have an account? <a className={styles.link} href="/signup">Sign up</a> to request access.
           </h3>
         </>
       }
