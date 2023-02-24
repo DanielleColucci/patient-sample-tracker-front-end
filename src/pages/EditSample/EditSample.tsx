70b2fd5f42be60ace87d62bc8e28ab4620a95a7e
DanielleColucci <ddcolucci99@gmail.com>
2023-02-24 10:41:44 -0500

    stub up EditSample page component

---
+++ src/pages/EditSample/EditSample.tsx
@@ -0,0 +1,68 @@
+// types 
+import { Sample } from "../../types/models"
+
+interface EditSampleProps {
+  sample: Sample;
+}
+
+const EditSample = (props: EditSampleProps): JSX.Element => {
+  return (
+    <main>
+      <h1>New Sample</h1>
+      <form>
+        <label htmlFor="MRN-input">MRN:</label>
+        <input
+          required
+          type="number" 
+          name="MRN"
+          id="MRN-input"
+          placeholder="MRN number"
+          autoComplete="off"
+        />
+        <label htmlFor="sample-number-input">Sample Number:</label>
+        <input
+          required
+          type="text" 
+          name="sampleNumber"
+          id="sample-number-input"
+          placeholder="Sample number"
+          autoComplete="off"
+        />
+        <label htmlFor="date-input">Date:</label>
+        <input
+          required
+          type="text" 
+          name="date"
+          id="date-input"
+          placeholder="YYYY-MM-DD"
+          autoComplete="off"
+        />
+        <label htmlFor="cell-line-select">Cell Line Status:</label>
+        <select 
+          required
+          name="cellLine" 
+          id="cell-line-select"
+        >
+          <option value="NA">NA</option>
+          <option value="In Process">In Process</option>
+          <option value="Finished">Finished</option>
+          <option value="Failed">Failed</option>
+        </select>
+        <label htmlFor="PDX-model-select">PDX Model Status:</label>
+        <select 
+          required
+          name="PDXModel" 
+          id="PDX-model-select"
+        >
+          <option value="NA">NA</option>
+          <option value="In Process">In Process</option>
+          <option value="Finished">Finished</option>
+          <option value="Failed">Failed</option>
+        </select>
+        <button type="submit">Submit</button>
+      </form>
+    </main>
+  )
+}
+
+export default EditSample
