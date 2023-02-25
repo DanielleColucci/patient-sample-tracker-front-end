603fb4091eea8060c6814f3fed320469042f2207
DanielleColucci <ddcolucci99@gmail.com>
2023-02-24 21:48:36 -0500

    update NewSample styling

--- src/pages/NewSample/NewSample.tsx
+++ src/pages/NewSample/NewSample.tsx
@@ -1,3 +1,5 @@
+import styles from './NewSample.module.css'
+
 import { useState } from "react"
 import { SampleFormData } from "../../types/forms"
 
@@ -24,70 +26,82 @@ const NewSample = (props: NewSampleProps): JSX.Element => {
   }
 
   return (
-    <main>
-      <h1>New Sample</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="MRN-input">MRN:</label>
-        <input
-          required
-          type="number" 
-          name="MRN"
-          id="MRN-input"
-          value={form.MRN}
-          placeholder="MRN number"
-          autoComplete="off"
-          onChange={handleChange}
-        />
-        <label htmlFor="sample-number-input">Sample Number:</label>
-        <input
-          required
-          type="text" 
-          name="sampleNumber"
-          id="sample-number-input"
-          value={form.sampleNumber}
-          placeholder="Sample number"
-          autoComplete="off"
-          onChange={handleChange}
-        />
-        <label htmlFor="date-input">Date:</label>
-        <input
-          required
-          type="text" 
-          name="date"
-          id="date-input"
-          value={form.date}
-          placeholder="YYYY-MM-DD"
-          autoComplete="off"
-          onChange={handleChange}
-        />
-        <label htmlFor="cell-line-select">Cell Line Status:</label>
-        <select 
-          required
-          name="cellLine" 
-          id="cell-line-select"
-          value={form.cellLine}
-          onChange={handleChange}
-        >
-          <option value="NA">NA</option>
-          <option value="In Process">In Process</option>
-          <option value="Finished">Finished</option>
-          <option value="Failed">Failed</option>
-        </select>
-        <label htmlFor="PDX-model-select">PDX Model Status:</label>
-        <select 
-          required
-          name="PDXModel" 
-          id="PDX-model-select"
-          value={form.PDXModel}
-          onChange={handleChange}
-        >
-          <option value="NA">NA</option>
-          <option value="In Process">In Process</option>
-          <option value="Finished">Finished</option>
-          <option value="Failed">Failed</option>
-        </select>
-        <button type="submit">Submit</button>
-      </form>
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <h1>New Sample</h1>
+        <form onSubmit={handleSubmit}>
+          <div className={styles.inputContainer}>
+            <label htmlFor="MRN-input">MRN:</label>
+            <input
+              required
+              type="number" 
+              name="MRN"
+              id="MRN-input"
+              value={form.MRN}
+              placeholder="MRN number"
+              autoComplete="off"
+              onChange={handleChange}
+            />
+          </div>
+          <div className={styles.inputContainer}>
+            <label htmlFor="sample-number-input">Sample Number:</label>
+            <input
+              required
+              type="text" 
+              name="sampleNumber"
+              id="sample-number-input"
+              value={form.sampleNumber}
+              placeholder="Sample number"
+              autoComplete="off"
+              onChange={handleChange}
+            />
+          </div>
+          <div className={styles.inputContainer}>
+            <label htmlFor="date-input">Date:</label>
+            <input
+              required
+              type="text" 
+              name="date"
+              id="date-input"
+              value={form.date}
+              placeholder="YYYY-MM-DD"
+              autoComplete="off"
+              onChange={handleChange}
+            />
+          </div>
+          <div className={styles.inputContainer}>
+            <label htmlFor="cell-line-select">Cell Line Status:</label>
+            <select 
+              required
+              name="cellLine" 
+              id="cell-line-select"
+              value={form.cellLine}
+              onChange={handleChange}
+            >
+              <option value="NA">NA</option>
+              <option value="In Process">In Process</option>
+              <option value="Finished">Finished</option>
+              <option value="Failed">Failed</option>
+            </select>
+          </div>
+          <div className={styles.inputContainer}>
+            <label htmlFor="PDX-model-select">PDX Model Status:</label>
+            <select 
+              required
+              name="PDXModel" 
+              id="PDX-model-select"
+              value={form.PDXModel}
+              onChange={handleChange}
+            >
+              <option value="NA">NA</option>
+              <option value="In Process">In Process</option>
+              <option value="Finished">Finished</option>
+              <option value="Failed">Failed</option>
+            </select>
+          </div>
+          <button className={styles.button} type="submit">SUBMIT</button>
+        </form>
+      </div>
     </main>
   )
 }
